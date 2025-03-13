e494992c0567ecad03efbfdb30612d717a41add0
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-03-13 20:29:58 -0300

    feat: implements and includes Contacts component and your style

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
+<template>
+    <MenuBar/>
+    <Banner/>
+    <About/>
+    <Contacts/>
+    <Footer/>
+</template>
+
 <script setup>
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
-import BarraMenu from './components/MenuBar.vue';
-import { useToast } from 'primevue/usetoast';
 import Button from 'primevue/button'
 import Toast from 'primevue/toast'
+
+import BarraMenu from './components/MenuBar.vue';
+import { useToast } from 'primevue/usetoast';
 import MenuBar from './components/MenuBar.vue';
 import About from './components/About.vue';
 import Banner from './components/Banner.vue';
 import Footer from './components/Footer.vue';
+import Contacts from './components/Contacts.vue';
 
 const toast = useToast();
 
 </script>
   
-<template>
-    <MenuBar/>
-    <Banner/>
-    <About/>
-    <Footer/>
-</template>
---
+++ src/components/Contacts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contacts">
+    <h2>Contact me</h2>
+    <div class="social">
+      <a href="https://www.linkedin.com/in/edvaldo-oliveira-14687b101/" target="_blank"><img src="../assets/social/linkedin_white.png" alt=""></a>
+      <a href="https://www.instagram.com" target="_blank"><img src="../assets/social/insta_white.png" alt=""></a>
+      <a href="https://wa.me/+5585999107495" target="_blank"><img src="../assets/social/whatsapp_white.png" alt=""></a>
+      <a href="https://t.me/feojunior" target="_blank"><img src="../assets/social/telegram_white.png" alt=""></a>
+    </div>
+    <div class="contact-form">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contacts',
+}
+</script> 
