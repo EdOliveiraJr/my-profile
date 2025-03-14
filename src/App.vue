a13f6d7365446454d49f0bc381a032faf800fd1d
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-03-14 14:55:28 -0300

    feat: removes unnecessary import from App component and updates styles

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,11 @@
 </template>
 
 <script setup>
-import InputText from 'primevue/inputtext';
-import FloatLabel from 'primevue/floatlabel';
-import Button from 'primevue/button'
-import Toast from 'primevue/toast'
-
-import BarraMenu from './components/MenuBar.vue';
-import { useToast } from 'primevue/usetoast';
 import MenuBar from './components/MenuBar.vue';
 import About from './components/About.vue';
 import Banner from './components/Banner.vue';
 import Footer from './components/Footer.vue';
 import Contacts from './components/Contacts.vue';
 import Projects from './components/Projects.vue';
-
-const toast = useToast();
-
 </script>
   
