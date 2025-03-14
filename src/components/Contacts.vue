88d7ae8c567a43d69d3844382f3dedd1109d20fd
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-03-14 15:04:34 -0300

    fix: solve style problems

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="contacts" class="contacts">
     <Toast/>
-    <h2>Contact me</h2>
     <div class="social">
-      <a href="https://www.linkedin.com/in/edvaldo-oliveira-14687b101/" target="_blank"><img
-          src="../assets/social/linkedin_white.png" alt=""></a>
-      <a href="https://www.instagram.com" target="_blank"><img src="../assets/social/insta_white.png" alt=""></a>
-      <a href="https://wa.me/+5585999107495" target="_blank"><img src="../assets/social/whatsapp_white.png" alt=""></a>
-      <a href="https://t.me/feojunior" target="_blank"><img src="../assets/social/telegram_white.png" alt=""></a>
+      <h2>Contact me</h2>
+      <div class="social-links">
+        <a href="https://www.linkedin.com/in/edvaldo-oliveira-14687b101/" target="_blank"><img
+            src="../assets/social/linkedin_white.png" alt=""></a>
+        <a href="https://www.instagram.com" target="_blank"><img src="../assets/social/insta_white.png" alt=""></a>
+        <a href="https://wa.me/+5585999107495" target="_blank"><img src="../assets/social/whatsapp_white.png" alt=""></a>
+        <a href="https://t.me/feojunior" target="_blank"><img src="../assets/social/telegram_white.png" alt=""></a>
+      </div>
     </div>
     <div class="contact-form">
       <div class="p-fluid">
